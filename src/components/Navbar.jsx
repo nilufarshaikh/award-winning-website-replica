dea24225586db9341de8323aa2f2438db7c1041e
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-25 16:36:22 -0500

    Updated navigation bar to appear on scroll up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,47 @@
 import React, {useEffect, useRef, useState} from 'react'
 import {TiLocationArrow} from "react-icons/ti";
 import Button from "./Button.jsx";
+import {useWindowScroll} from "react-use";
+import gsap from "gsap";
 
 const Navbar = () => {
-    const [isAudioPlaying, setIsAudioPlaying] = React.useState(false);
-    const [isIndicatorActive, setIsIndicatorActive] = React.useState(false);
+    const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+    const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isNavVisible, setIsNavVisible] = useState(true);
 
     const navContainerRef = useRef(null);
     const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
     const audioElementRef = useRef(null);
+
+    const {y: currentScrollY} = useWindowScroll()
+
+    useEffect(() => {
+        if (currentScrollY === 0) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.remove('floating-nav');
+        } else if(currentScrollY > lastScrollY) {
+            setIsNavVisible(false);
+            navContainerRef.current.classList.add('floating-nav');
+        } else if (currentScrollY < lastScrollY) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.add('floating-nav');
+        }
+
+        setLastScrollY(currentScrollY);
+    }, [currentScrollY, lastScrollY]);
+
+    useEffect(() => {
+        gsap.to(navContainerRef.current, {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2
+        })
+    }, [isNavVisible]);
+
+
     const toggleAudioIndicator = () => {
         setIsAudioPlaying((prev) => !prev);
         setIsIndicatorActive((prev) => !prev);
