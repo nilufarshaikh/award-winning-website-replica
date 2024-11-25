da87ef0ba069c43d7557b78e55f452b95c2a718a
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-25 14:58:30 -0500

    Added animated title for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import {useGSAP} from "@gsap/react";
 
 import {ScrollTrigger} from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle.jsx";
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
@@ -31,9 +32,8 @@ const About = () => {
                 <h2 className="font-general text-sm uppercase md:text-[10px]">
                     Welcome to Zentry
                 </h2>
-                <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                    Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure
-                </div>
+
+                <AnimatedTitle title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center"/>
 
                 <div className="about-subtext">
                     <p>The Game of Games begins-your life, now an epic MMORPG</p>
