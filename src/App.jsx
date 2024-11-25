737b6fd0a801e2cc3f11e959043514111d7e658f
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-25 15:56:57 -0500

    Added navbar and audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar/>
             <Hero/>
             <About/>
             <section className="z-0 min-h-screen bg-blue-500" />
