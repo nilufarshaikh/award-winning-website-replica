74ec4baf482ec50afb0fb4c96df6ab8472fe1411
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-25 17:22:16 -0500

    Added Story section below bento grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 const App = () => {
     return (
@@ -10,8 +11,10 @@ const App = () => {
             <Hero/>
             <About/>
             <Features/>
+            <Story/>
             <section className="z-0 min-h-screen bg-blue-500" />
         </main>
     )
 }
+
 export default App
