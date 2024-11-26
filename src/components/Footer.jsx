84171a7231cd35cbcb7ebc26afd0b13b739eb743
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-11-25 22:00:12 -0500

    fix minor bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const socialLinks = [
 const Footer = () => {
     return (
         <footer className="w-screen bg-violet-300 py-4 text-black">
-            <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+            <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-10 md:flex-row">
                 <p className="text-center text-sm font-light md:text-left">
                     &copy; SuperNova 2024. All rights reserved
                 </p>
